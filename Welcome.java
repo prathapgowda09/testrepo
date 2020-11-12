f6476ca384a1b9d33a8e0899e737257236281268
phrathap <prathapgowda09@gmail.com>
2020-11-12 22:53:24 +0530

    pizza and juice commit

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,14 @@
 class Welcome {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+		
+		boolean flag;
+		
+		if(flag){
+			System.out.println("Pizza")
+		}else{
+			System.out.println("juice")
+		}
+		
     }
 }
